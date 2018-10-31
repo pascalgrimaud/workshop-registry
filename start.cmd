a1db22c4d8ad84aa297dd03dd969c16fd46d4471
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-10-31 11:57:03 +0100

    init repo for registry without docker

---
+++ start.cmd
@@ -0,0 +1,1 @@
+java -jar jhipster-registry-4.0.4.war --spring.profiles.active=dev --spring.security.user.password=admin --jhipster.security.authentication.jwt.secret=my-secret-key-which-should-be-changed-in-production-and-be-base64-encoded
